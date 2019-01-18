0ca6891e827764293e009b70372b90ea3906c8b3
Unknown <yifengyang01@gmail.com>
2019-01-18 18:52:17 -0500

    initial intellij commit

---
+++ src/main/java/com/yfy/hx/micreaumanagement/MicreauManagementApplication.java
@@ -0,0 +1,14 @@
+package com.yfy.hx.micreaumanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicreauManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicreauManagementApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/yfy/hx/micreaumanagement/MicreauManagementApplicationTests.java
@@ -0,0 +1,17 @@
+package com.yfy.hx.micreaumanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicreauManagementApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
