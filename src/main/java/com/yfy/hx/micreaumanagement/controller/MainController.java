a445115f36c584a266c493ac973cc4241adda8f4
Unknown <yifengyang01@gmail.com>
2019-01-18 19:27:03 -0500

    added temperature randomizer

--- src/main/java/com/yfy/hx/micreaumanagement/controller/MainController.java
+++ src/main/java/com/yfy/hx/micreaumanagement/controller/MainController.java
@@ -1,6 +1,9 @@
 package com.yfy.hx.micreaumanagement.controller;
 
 import com.yfy.hx.micreaumanagement.randomizers.PhRandomizer;
+import com.yfy.hx.micreaumanagement.randomizers.TemperatureRandomizer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,16 +11,35 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
-    @Autowired
-    private PhRandomizer rand;
+    private static final Logger log = LoggerFactory.getLogger(MainController.class);
 
-    @GetMapping("/")
+    private PhRandomizer phRand;
+    private TemperatureRandomizer tRand;
+
+    @Autowired
+    public MainController(PhRandomizer phRand, TemperatureRandomizer tRand) {
+        this.phRand = phRand;
+        this.tRand = tRand;
+    }
+
+    @GetMapping("ph")
     @ResponseBody
-    public String getRand() {
+    public String getPh() {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < 10; i++) {
-            sb.append("normal value: " + rand.getNextPhNormal() + " ");
-            sb.append("abn value: " + rand.getNextPhSpike() + "        ");
+            sb.append("normal ph: " + phRand.getNextPhNormal() + " ");
+            sb.append("abn ph: " + phRand.getNextPhSpike() + "        ");
+        }
+        return sb.toString();
+    }
+
+    @GetMapping("temp")
+    @ResponseBody
+    public String getTemp() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 10; i++) {
+            sb.append("normal t: " + tRand.getNextTempNormal() + " ");
+            sb.append("abn t: " + tRand.getNextTempSpike() + "        ");
         }
         return sb.toString();
     }
--- src/main/java/com/yfy/hx/micreaumanagement/randomizers/TemperatureRandomizer.java
+++ src/main/java/com/yfy/hx/micreaumanagement/randomizers/TemperatureRandomizer.java
@@ -6,24 +6,23 @@ import java.text.DecimalFormat;
 import java.util.Random;
 
 @Component
-public class PhRandomizer {
-    public static final double LOWER = 6.5;
-    public static final double HIGHER = 7.5;
+public class TemperatureRandomizer {
+    public static final int LOWER = 20;
+    public static final int HIGHER = 30;
 
     private final Random r = new Random();
     private final DecimalFormat db = new DecimalFormat("#.#");
 
-    public double getNextPhNormal() {
+    public double getNextTempNormal() {
         double d = (LOWER + (HIGHER - LOWER) * r.nextDouble());
         return Double.parseDouble(db.format(d));
     }
 
-    public double getNextPhSpike() {
+    public double getNextTempSpike() {
         double d = Double.parseDouble(db.format(r.nextDouble() * 14));
         if (d >= LOWER && d <= HIGHER) {
-            return getNextPhSpike();
+            return getNextTempNormal();
         }
         return d;
     }
-
 }
