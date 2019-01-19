38fe294be1fcccac85c35f9973998647fd5c26c7
Unknown <yifengyang01@gmail.com>
2019-01-18 20:19:56 -0500

    added redirection from home page

---
+++ src/main/java/com/yfy/hx/micreaumanagement/config/WebConfiguration.java
@@ -0,0 +1,13 @@
+package com.yfy.hx.micreaumanagement.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/","home");
+    }
+}
