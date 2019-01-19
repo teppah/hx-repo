3fb4a765ec5814cbaa5d755568193cad7b66e6a9
Unknown <yifengyang01@gmail.com>
2019-01-18 23:24:18 -0500

    get ctx of springapplication

--- src/main/java/com/yfy/hx/micreaumanagement/MicreauManagementApplication.java
+++ src/main/java/com/yfy/hx/micreaumanagement/MicreauManagementApplication.java
@@ -2,12 +2,13 @@ package com.yfy.hx.micreaumanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MicreauManagementApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MicreauManagementApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(MicreauManagementApplication.class, args);
     }
 
 }
